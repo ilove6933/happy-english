dcda0ddf4bb5494d13831670f44f54d24cfc783c
ilove6933 <ilove6933@gmail.com>
2025-12-15 12:20:20 +0800

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+// import './index.css'  <-- 如果您沒有這個檔案，請確認這行是被刪除或註解掉的，不然也會報錯白畫面
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
